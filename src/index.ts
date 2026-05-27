49bcb7ca5100be017c5e64207f81af07ff0d41de
nicoeltes <nicolaseltes@gmail.com>
2026-05-27 15:37:59 -0300

    cambios en el ts

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,10 @@ let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
-let totalPorPersona: number = Number(totalTexto) / Number(personasTexto) ; // COMPLETAR
+let totalPorPersona: number = Number(totalTexto) / Number(personasTexto) ;
+
+if (esEstudiante === "si"){
+    totalPorPersona = totalPorPersona * 0.9;
+}
+
 console.log(`Total por persona: $${totalPorPersona}`);
